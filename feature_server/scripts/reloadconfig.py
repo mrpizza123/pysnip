c1b6d4a11f7d64e9936c66c62f900ad0d66898c3
colorpinpoint <colorpinpoint@hotmail.com>
2013-01-15 11:51:52 -0500

    Update feature_server/scripts/reloadconfig.py
    Better reload config.

--- feature_server/scripts/reloadconfig.py
+++ feature_server/scripts/reloadconfig.py
@@ -1,4 +1,3 @@
-import commands
 from commands import add, admin
 import json
 
@@ -13,20 +12,9 @@ def reloadconfig(connection):
         print 'Error reloading config:', v
         return 'Error reloading config. Check pyspades log for details.'
     connection.protocol.config.update(new_config)
-    connection.protocol.reload_passes()
     return 'Config reloaded!'
 
 add(reloadconfig)
 
 def apply_script(protocol, connection, config):
-    class PassreloadProtocol(protocol):
-        def reload_passes(self):
-            self.passwords = config.get('passwords', {})
-            for password in self.passwords.get('admin', []):
-                if password == 'replaceme':
-                    print 'REMEMBER TO CHANGE THE DEFAULT ADMINISTRATOR PASSWORD!'
-                elif not password:
-                    self.everyone_is_admin = True
-            commands.rights.update(config.get('rights', {}))
-            script_names = config.get('scripts', [])
-    return PassreloadProtocol, connection
+    return protocol, connection
