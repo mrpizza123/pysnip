5dcbfa5adb861f385828dbbf9311dbcb60046e19
colorpinpoint <colorpinpoint@hotmail.com>
2013-01-27 18:21:30 -0500

    scrap that

--- feature_server/scripts/alias.py
+++ feature_server/scripts/alias.py
@@ -5,9 +5,9 @@ import re
 deuce_test_name_pattern = re.compile("deuce([0-9]$|[1-2][0-9]|3[0-1])$")
 duplicate_test_name_pattern = re.compile(".*(\d)$")
 
-@alias('namealias')
+@alias('aliases')
 @admin
-def namealias(connection, value):
+def alias(connection, value):
 	try:
             	aliases = json.load(open('aliases.txt', 'rb'))
         except IOError:
@@ -21,24 +21,7 @@ def namealias(connection, value):
 			if len(aliaslist) == 0:
 				return 'No know aliases of %s' % player.name
 			return 'Known aliases of %s: %s' % (player.name,', '.join(map(str,aliaslist)))
-add(namealias)
-
-@alias('ipalias')
-def alias(connection, value):
-	try:
-            	aliases = json.load(open('aliases.txt', 'rb'))
-        except IOError:
-            	aliases = []
-	ip = value
-	for ip in aliases:
-		if player.address[0] == ip[0]:
-			del ip[0]
-	        aliaslist = filter( lambda a: a.lower() != player_name and (duplicate_test_name_pattern.match(a) is not None or a != player_name[:-1]), ip)
-	    if len(aliaslist) == 0:
-		    return 'No know aliases of %s' % ip
-	    return 'Known aliases of %s: %s' % (ip,', '.join(map(str,aliaslist)))
-add(ipalias)
-
+add(aliases)
 
 def apply_script(protocol, connection, config):
     class Alias(connection):
