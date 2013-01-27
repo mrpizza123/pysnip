184b639b79d5ccb7794263cf03cf4b5027cbe6f1
colorpinpoint <colorpinpoint@hotmail.com>
2013-01-27 12:08:10 -0500

    asfa

--- feature_server/commands.py
+++ feature_server/commands.py
@@ -967,6 +967,31 @@ def hp(connection, player_name):
         return 'Player is a spectator'
     
     return "%s's HP is: %i" % (player.name, player.hp)
+
+@alias('staff')
+@admin
+def staff(connection):
+    protocol = connection.protocol
+    admin_message = "Admins logged in: "
+    for conn in protocol.connections.values():
+        if conn.admin:
+            admin_message += "%s [A], " % conn.name
+        elif conn.user_types is not None:
+            if conn.user_types.moderator:
+                admin_message += "%s [M], " % conn.name
+            elif conn.user_types.guard:
+                admin_message += "%s [G], " % conn.name
+            elif conn.user_types.trusted:
+                admin_message += "%s [T], " % conn.name
+    if admin_message == "Admins logged in: ":
+        admin_message = "No admins are logged in."
+    else:
+        admin_message = admin_message[:-2]
+        if connection not in connection.protocol.players:
+            admin_message += "\n [A]dmin [M]oderator [G]uard [T]rusted"
+        else:
+            connection.send_chat("[A]dmin [M]oderator [G]uard [T]rusted")
+    return admin_message	
     
 command_list = [
     help,
@@ -1032,7 +1057,8 @@ command_list = [
     scripts,
     weapon,
     mapname,
-    hp
+    hp,
+	staff
 ]
 
 def add(func, name = None):
